fcf2df7f360db996bb7957b257d61d002c6c013d
Ziad Belhadri <ziadbelh17@gmail.com>
2023-03-27 22:33:25 +0000

    my second push

--- traitementOtherwise.php
+++ traitementOtherwise.php
@@ -3,6 +3,28 @@
 <head>
     <style>body{
     text-align: center;
+}
+#content {
+  display: none;
+}
+input[type="text"]{
+    color: transparent;
+    text-shadow: 0 0 0 #000;
+    padding: 6px 12px;
+    width: 150px;
+    cursor: pointer;
+}
+input[type="text"]:focus{
+    outline: none;
+}
+#text{
+    font-size: 16px;
+}
+input:focus + div#content {
+  display: block;
+}
+input[type="text"] {
+  color: green;
 }</style>
 <title>Guess the number</title>
   </head>
@@ -39,28 +61,59 @@ $_SESSION['score'] = 20;}
 
   $_SESSION['number'] = rand(1, $_SESSION['till']);
 }
-
-  if (isset($_POST['guess'])) {
+if(($_SESSION['restart']==1))
+    {
+        $_SESSION['number'] = rand(1, $_SESSION['till']);
+        $_SESSION['message'] = "";
+        $_SESSION['score'] = 20;
+        $_SESSION['restart']=0;
+        $_SESSION['nbr_guesses'] = $_SESSION['nbr_tries']+1;
+    
+    }
+if (isset($_POST['guess'])) {
     $_SESSION['guess'] = $_POST['guess'];
     $_SESSION['nbr_guesses'] = $_SESSION['nbr_guesses'] - 1;
     if ($_SESSION['nbr_guesses'] > 0) {
         if ($_SESSION['guess'] == $_SESSION['number']) {
             $_SESSION['message'] = "<div>You guessed the number! Score :" . $_SESSION['score'] . "</div>";
+            $success=1;
+            $finish=1;
+            $_SESSION['restart']=1;
+            
         }
         else if ($_SESSION['guess'] > $_SESSION['number']) {
-            $_SESSION['score'] = $_SESSION['score'] -  20 / $_SESSION['nbr_tries'];
             $_SESSION['message'] = "<div>Your guess (".$_SESSION['guess'].") is too high! Score :".$_SESSION['score']."</div>";
-        } else if ($_SESSION['guess'] < $_SESSION['number']) {
             $_SESSION['score'] = $_SESSION['score'] -  20 / $_SESSION['nbr_tries'];
+        } else if ($_SESSION['guess'] < $_SESSION['number']) {
             $_SESSION['message'] = "<div>Your guess (".$_SESSION['guess'].") is too low! Score :".$_SESSION['score']."</div>";
+            $_SESSION['score'] = $_SESSION['score'] -  20 / $_SESSION['nbr_tries'];
         }
         
     }
-    elseif($_SESSION['nbr_guesses']<0){
+    elseif($_SESSION['nbr_guesses']==0){
         $_SESSION['message']="<div>you have no tries left,".$_SESSION['score']."</div>";
         $_SESSION['score'] = $_SESSION['score'] -   20 / $_SESSION['nbr_tries'];
-
+        $success=0;
+        $finish=1;
+        $_SESSION['restart']=1;
+        
+        
     }
+    
+    if (isset($success)){
+    if ($finish == 1) 
+        {
+            $nameButton = "Restart";
+        }
+    else{
+        $nameButton = "Guess";}}
+else{
+    $nameButton = "Guess";
+}
+
+
+
+    
 
 
 }
@@ -76,17 +129,18 @@ $_SESSION['score'] = 20;}
         <h1> You have choosed the difficulty <?php echo $_SESSION['level'] ;?></h1>
         <h1> You still have : <?php echo $_SESSION['nbr_guesses'] ?> guesses</h1>
         <input type="number" name="guess" id="guess">
-        <input type="submit" value="guess">
+        <input type="submit" value=" <?php echo $nameButton; ?>">
         </div>
                                         <?php
                                         if (isset($_SESSION['message'])) {
                                             echo $_SESSION['message'];
-                                        }
+                                        }?> </div> <input type="text" id='text'value="show the answer ">
+                                            <div id='content'><?php
                                         echo"<br>".$_SESSION['number'];
                                         ?>
-                                    </div>
-        
-      </form>
+                                        </div>
+                                    
+                                </form>
     </body>
     </div>
 
